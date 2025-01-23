f486bac59866eeed71a1cde27e29160f68e2efed
itzpcgamerhasan <140355843+itzpcgamerhasan@users.noreply.github.com>
2025-01-23 17:07:08 +0600

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKx_SDYuKMMxumQ0-LurDKFuJealqdA7U",
+  authDomain: "dppushn-otification.firebaseapp.com",
+  projectId: "dppushn-otification",
+  storageBucket: "dppushn-otification.firebasestorage.app",
+  messagingSenderId: "656610319409",
+  appId: "1:656610319409:web:7519cbd4de540f130f0910",
+  measurementId: "G-GE6TR9Q6V4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log('Background message received: ', payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  });
+});
